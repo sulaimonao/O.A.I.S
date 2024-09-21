59253bd002fff6397ee3ba4f61a905c1cdd17d57
Akeem <16595684+sulaimonao@users.noreply.github.com>
2024-09-21 10:15:10 -0700

    reorganizing

--- tools/devices/bluetooth_handler.py
+++
@@ -1,9 +0,0 @@
-import bluetooth
-
-def search_bluetooth_devices():
-    devices = bluetooth.discover_devices(lookup_names=True)
-    return devices
-
-def connect_bluetooth_device(device_address):
-    # Logic to connect to the selected Bluetooth device
-    pass
--- tools/devices/usb_handler.py
+++
@@ -1,9 +0,0 @@
-import serial.tools.list_ports
-
-def list_usb_devices():
-    ports = serial.tools.list_ports.comports()
-    return [port.device for port in ports]
-
-def connect_to_device(device_name):
-    # Logic to connect to the selected USB device
-    pass
